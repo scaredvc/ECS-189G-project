deca0b7a32eaede5844a5e513e61830a06a967bc
DZ <ansontan635@gmail.com>
2025-04-24 20:29:28 -0700

    start stage 2 and add data

---
+++ script/stage_2_script/main.py
@@ -0,0 +1,18 @@
+from local_code.stage_2_code.Dataset_Loader import Dataset_Loader
+from local_code.stage_2_code.Method_MLP import Method_MLP
+from local_code.stage_2_code.Result_Saver import Result_Saver
+from local_code.stage_2_code.Setting_KFold_CV import Setting_KFold_CV
+from local_code.stage_2_code.Setting_Train_Test_Split import Setting_Train_Test_Split
+from local_code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
+import numpy as np
+import torch
+
+data_path = '../data/stage_2_data/'
+
+training_data = Dataset_Loader('stage2data_train', '')
+training_data.dataset_source_folder_path = data_path
+training_data.dataset_source_file_name = 'train.csv'
+
+test_data = Dataset_Loader('stage2data_test', '')
+test_data.dataset_source_folder_path = data_path
+test_data.dataset_source_file_name = 'test.csv'
